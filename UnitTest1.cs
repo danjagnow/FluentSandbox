8343dc1e3a13aa316d0e2e324e49295ba802c948
Dan Jagnow <dan.jagnow@gmail.com>
2018-01-25 14:01:11 -0600

    Added a minimal solution to reproduce an issue

---
+++ UnitTest1.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using FluentAssertions;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace FluentSandbox
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void WorksOnNetFrameworkButFailsOnCore()
+        {
+            const string json = @"{
+                ""NestedDictionary"": {
+                    ""StringProperty"": ""string"",
+                    ""IntProperty"": 123
+                }
+            }";
+            
+            var expectedResult = new Dictionary<string, object>
+            {
+                ["NestedDictionary"] = new Dictionary<string, object>
+                {
+                    ["StringProperty"] = "string",
+                    ["IntProperty"] = 123
+                }
+            };
+
+            var result = JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
+            result.ShouldBeEquivalentTo(expectedResult);
+        }
+
+        [Fact]
+        public void WorksOnBoth()
+        {
+            const string json = @"{
+                    ""StringProperty"": ""string"",
+                    ""IntProperty"": 123
+                }";
+
+            var expectedResult = new Dictionary<string, object>
+            {
+                ["StringProperty"] = "string",
+                ["IntProperty"] = 123
+            };
+
+            var result = JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
+            result.ShouldBeEquivalentTo(expectedResult);
+        }
+    }
+}
